9c4c3250ae3703779ebfe765f3bb21b03856d071
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-05-24 21:43:34 +0400

    Dummy implementation of session. Attributes go into /dev/null

--- src/ring_userprincipal_middleware/dev.clj
+++ src/ring_userprincipal_middleware/dev.clj
@@ -1,14 +1,19 @@
 (ns ring-userprincipal-middleware.dev
     (:import javax.servlet.http.HttpServletRequest
+             javax.servlet.http.HttpSession
              java.security.Principal))
 
 
-(defn- wrap-with-fake-user [handler fake-username fake-roles]
+(defn wrap-with-fake-user [app fake-username fake-roles]
   "Wrap request with fake user and fake roles. Useful while developing."
-  (let [fake-user (proxy [java.security.Principal][]
-		    (getName [] fake-username))
-	fake-request (proxy [javax.servlet.http.HttpServletRequest] []
-		       (getUserPrincipal [] fake-user)
-		       (isUserInRole [role] (contains? fake-roles role)))]
+  (let [dummy-session (proxy [HttpSession] []
+                        (getAttribute [key] nil)
+                        (setAttribute [key, val]))
+	fake-user (proxy [Principal] []
+                (getName [] fake-username))
+	fake-request (proxy [HttpServletRequest] []
+                (getSession [& create] dummy-session)
+	        (getUserPrincipal [] fake-user)
+                (isUserInRole [role] (contains? fake-roles role)))]
     (fn [request]
-      (handler (assoc request :servlet-request fake-request)))))
+      (app (assoc request :servlet-request fake-request)))))
