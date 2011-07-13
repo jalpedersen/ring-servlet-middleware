76c066d525775b682bb98ed8ca88b014f2cb7a12
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-07-14 00:51:23 +0400

    Remove direct ring dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.signaut/ring.middleware.servlet-ext "0.1"
+(defproject org.signaut/ring.middleware.servlet-ext "0.2"
   :description "Exposes information from the underlying servlet environment."
   :url "http://github.com/jalpedersen/ring-userprincipal-middleware"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ring/ring-servlet "0.3.8"]]
+                 [javax.servlet/servlet-api "2.5"]] 
   :dev-dependencies [[lein-clojars "0.6.0"]]
   :warn-on-reflection true)
