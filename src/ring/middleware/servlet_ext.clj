6565fd93dfdd2f104093d8840f3c7a61192a974b
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-07-14 00:47:24 +0400

    Add authorisation functionality

--- src/ring/middleware/servlet_ext.clj
+++ src/ring/middleware/servlet_ext.clj
@@ -2,20 +2,38 @@
   (:import javax.servlet.http.HttpServletRequest
            java.security.Principal))
 
-(defn wrap-userprincipal [app]
+(defn- in-role? [^HttpServletRequest servlet-req required-roles]
+  (if (empty? required-roles)
+    true
+    (loop [remaining-roles required-roles]
+      (if (empty? remaining-roles)
+        false
+        (if (.isUserInRole servlet-req (first remaining-roles))
+          true
+          (recur (rest remaining-roles)))))))
+
+(defn wrap-userprincipal [app & options]
   "Wrap request with user principal.
   If a userprincipal is available, request is associated with
   a user (:username) and a predicate that given a role name returns true
   or false depending on wheter the user is in that role or not."
-  (fn [req]
-    (let [^HttpServletRequest servlet-req (:servlet-request req)
-          ^Principal principal (if servlet-req 
-                      (.getUserPrincipal servlet-req)
-                      nil)]
-      (if principal 
-        (app (assoc req :username (.getName principal)
-                    :in-role? #(.isUserInRole servlet-req %)))
-        (app (assoc req :username nil
-                    :in-role? (fn [role] false)))))))
+  (let [required-roles (:required-roles (first options))]
+    (fn [req]
+      (let [^HttpServletRequest servlet-req (:servlet-request req)
+            ^Principal principal (if servlet-req
+                                   (.getUserPrincipal servlet-req)
+                                   nil)
+            error-response {:status 403
+                            :headers {"content-type" "text/plain"}
+                            :body "Not authenticated."}]
+        (if principal
+          (if (in-role? servlet-req required-roles)
+            (app (assoc req :username (.getName principal)
+                        :in-role? #(.isUserInRole servlet-req %)))
+            error-response)
+          (if required-roles
+            error-response
+            (app (assoc req :username nil
+                        :in-role? (fn [role] false)))))))))
 
-  
+
--- test/ring/middleware/servlet_ext/test/core.clj
+++ test/ring/middleware/servlet_ext/test/core.clj
@@ -3,5 +3,52 @@
         ring.middleware.servlet-ext.dev
         clojure.test))
 
+(defn- testapp [request]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :custom  {:valid-role ((:in-role? request) "valid")
+             :invalid-role ((:in-role? request) "invalid")}
+   :body    (:username request)})
+
+(def wrapped-no-auth
+  (-> #'testapp
+    (wrap-userprincipal)))
+
+(def wrapped-no-auth-no-principal
+  (-> #'testapp
+    (wrap-userprincipal {:required-roles []})))
+
+(def wrapped-no-roles
+  (-> #'testapp
+    (wrap-userprincipal {:required-roles []})
+    (wrap-with-fake-user "monty" #{"valid"})))
+
+(def wrapped-with-valid-roles
+  (-> #'testapp
+    (wrap-userprincipal {:required-roles ["valid"]})
+    (wrap-with-fake-user "monty" #{"valid"})))
+
+(def wrapped-with-invalid-roles
+  (-> #'testapp
+    (wrap-userprincipal {:required-roles ["not-valid"]})
+    (wrap-with-fake-user "monty" #{"valid"})))
+
 (deftest test1
-  (is true true))
+         (is (= 200 (:status (wrapped-no-auth {})))))
+
+(deftest test2
+         (is (= 403 (:status (wrapped-no-auth-no-principal {})))))
+
+(deftest test3
+         (let [response (wrapped-no-roles {})]
+           (is (= 200 (:status response)))
+           (is (= true ((:custom response) :valid-role)))
+           (is (= false ((:custom response) :invalid-role)))
+           (is (= "monty" (:body response)))))
+
+(deftest test4
+         (is (= 200 (:status (wrapped-with-valid-roles {})))))
+
+(deftest test5
+         (is (= 403 (:status (wrapped-with-invalid-roles {})))))
+
