223e77ec44ead8fd359af54fb41f3f62770c43b4
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-07-17 17:36:09 +0400

    Unroll arguments

--- src/ring/middleware/servlet_ext.clj
+++ src/ring/middleware/servlet_ext.clj
@@ -12,28 +12,27 @@
           true
           (recur (rest remaining-roles)))))))
 
-(defn wrap-userprincipal [app & options]
+(defn wrap-userprincipal [app & [ & {:keys [required-roles]}]]
   "Wrap request with user principal.
   If a userprincipal is available, request is associated with
   a user (:username) and a predicate that given a role name returns true
   or false depending on wheter the user is in that role or not."
-  (let [required-roles (:required-roles (first options))]
-    (fn [req]
-      (let [^HttpServletRequest servlet-req (:servlet-request req)
-            ^Principal principal (if servlet-req
-                                   (.getUserPrincipal servlet-req)
-                                   nil)
-            error-response {:status 403
-                            :headers {"content-type" "text/plain"}
-                            :body "Not authenticated."}]
-        (if principal
-          (if (in-role? servlet-req required-roles)
-            (app (assoc req :username (.getName principal)
-                        :in-role? #(.isUserInRole servlet-req %)))
-            error-response)
-          (if required-roles
-            error-response
-            (app (assoc req :username nil
-                        :in-role? (fn [role] false)))))))))
+  (fn [req]
+    (let [^HttpServletRequest servlet-req (:servlet-request req)
+          ^Principal principal (if servlet-req
+                                 (.getUserPrincipal servlet-req)
+                                 nil)
+          error-response {:status 403
+                          :headers {"content-type" "text/plain"}
+                          :body "Not authenticated."}]
+      (if principal
+        (if (in-role? servlet-req required-roles)
+          (app (assoc req :username (.getName principal)
+                      :in-role? #(.isUserInRole servlet-req %)))
+          error-response)
+        (if required-roles
+          error-response
+          (app (assoc req :username nil
+                      :in-role? (fn [role] false))))))))
 
 
