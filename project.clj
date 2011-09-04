accb51b1a77a4eccefd0811b77ab17414789e84c
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-09-04 21:33:45 +0400

    dummy session store

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.signaut/ring.middleware.servlet-ext "0.2"
+(defproject org.signaut/ring.middleware.servlet-ext "0.3"
   :description "Exposes information from the underlying servlet environment."
   :url "http://github.com/jalpedersen/ring-userprincipal-middleware"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [javax.servlet/servlet-api "2.5"]] 
-  :dev-dependencies [[lein-clojars "0.6.0"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]]
   :warn-on-reflection true)
--- src/ring/middleware/servlet_ext/dev.clj
+++ src/ring/middleware/servlet_ext/dev.clj
@@ -2,13 +2,13 @@
     (:import javax.servlet.http.HttpServletRequest
              javax.servlet.http.HttpSession
              java.security.Principal))
-
+(def *session-store* (atom {}))
 
 (defn wrap-with-fake-user [app fake-username fake-roles]
   "Wrap request with fake user and fake roles. Useful while developing."
   (let [dummy-session (proxy [HttpSession] []
-                        (getAttribute [key] nil)
-                        (setAttribute [key, val]))
+                        (getAttribute [key] (@*session-store* key))
+                        (setAttribute [key, val] (swap! *session-store* assoc key val)))
 	fake-user (proxy [Principal] []
                 (getName [] fake-username))
 	fake-request (proxy [HttpServletRequest] []
