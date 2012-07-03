91e499527122437819542404d6e97d89b70c5d62
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2012-07-03 22:35:42 +0200

    Clojure 1.4.0 Support for saving session in underlying http-session

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.signaut/ring.middleware.servlet-ext "0.5"
+(defproject org.signaut/ring.middleware.servlet-ext "0.6"
   :description "Exposes information from the underlying servlet environment."
   :url "http://github.com/jalpedersen/ring-userprincipal-middleware"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-core "1.1.0"]
                  [javax.servlet/servlet-api "2.5"]] 
-  :dev-dependencies [[lein-clojars "0.7.0"]]
+  :dev-dependencies [[lein-clojars "0.9.0"]]
   :warn-on-reflection true)
--- src/ring/middleware/servlet_ext.clj
+++ src/ring/middleware/servlet_ext.clj
@@ -1,6 +1,33 @@
 (ns ring.middleware.servlet-ext
-  (:import javax.servlet.http.HttpServletRequest
+  (:use ring.middleware.session.store)
+  (:import [javax.servlet.http HttpServletRequest HttpSession]
+           java.util.UUID
            java.security.Principal))
+
+(declare ^:private ^:dynamic ^HttpSession *session*)
+
+(defn- assert-session []
+  (assert *session* "session is not bound. Please include wrap-with-session middleware."))
+
+(deftype HttpSessionStore [session-key]
+  SessionStore
+  (read-session [store key]
+                (assert-session)
+                (or (.getAttribute *session* key) {}))
+  (write-session [store key data]
+                 (assert-session)
+                 (let [key (or key (str (UUID/randomUUID)))]
+                   (.setAttribute *session* key data)
+                   key))
+  (delete-session [store key]
+                  (assert-session)
+                  (.removeAttribute *session* key)
+                  nil))
+
+(defn httpsession-store
+  ([] (httpsession-store "_http_session"))
+  ([session-key] (HttpSessionStore. session-key)))
+
 
 (defn- in-role? [^HttpServletRequest servlet-req allow-roles]
   (if (empty? allow-roles)
@@ -14,20 +41,20 @@
 
 (defn without-contextpath [handler]
   "Remove leading context path from URI"
-  (fn [req]
-    (let [^HttpServletRequest servlet-req (:servlet-request req)]
+  (fn [request]
+    (let [^HttpServletRequest servlet-req (:servlet-request request)]
       (if servlet-req
-        (handler (assoc req :uri (.substring (.getRequestURI servlet-req)
-                                             (.length (.getContextPath servlet-req)))))
-        (handler req)))))
+        (handler (assoc request :uri (.substring (.getRequestURI servlet-req)
+                                                 (.length (.getContextPath servlet-req)))))
+        (handler request)))))
 
-(defn wrap-userprincipal [app & [ & {:keys [allow-roles]}]]
+(defn wrap-userprincipal [handler & [ & {:keys [allow-roles]}]]
   "Wrap request with user principal.
   If a userprincipal is available, request is associated with
   a user (:username) and a predicate that given a role name returns true
   or false depending on wheter the user is in that role or not."
-  (fn [req]
-    (let [^HttpServletRequest servlet-req (:servlet-request req)
+  (fn [request]
+    (let [^HttpServletRequest servlet-req (:servlet-request request)
           ^Principal principal (if servlet-req
                                  (.getUserPrincipal servlet-req)
                                  nil)
@@ -36,12 +63,17 @@
                           :body "Not authorized."}]
       (if principal
         (if (in-role? servlet-req allow-roles)
-          (app (assoc req :username (.getName principal)
-                      :in-role? #(.isUserInRole servlet-req %)))
+          (handler (assoc request :username (.getName principal)
+                          :in-role? #(.isUserInRole servlet-req %)))
           error-response)
         (if allow-roles
           error-response
-          (app (assoc req :username nil
-                      :in-role? (fn [role] false))))))))
+          (handler (assoc request :username nil
+                          :in-role? (fn [role] false))))))))
 
-
+(defn wrap-with-session [handler]
+  (fn [request]
+    (if-let [^HttpServletRequest servlet-req (:servlet-request request)]
+      (binding [*session* (.getSession servlet-req)]
+        (handler request))
+      (handler request))))
