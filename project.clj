97f8a27dc25dacc8a6aeb658bda9c19b47582510
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2013-09-11 21:26:33 +0200

    ring 1.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject org.signaut/ring.middleware.servlet-ext "0.9"
+(defproject org.signaut/ring.middleware.servlet-ext "1.0"
             :description "Exposes information from the underlying servlet environment."
             :url "http://github.com/jalpedersen/ring-userprincipal-middleware"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [ring/ring-core "1.1.6"]]
-            :plugins [[lein-clojars "0.9.0"]]
-            :warn-on-reflection true)
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [ring/ring-core "1.2.0"]]
+            :plugins [[lein-clojars "0.9.1"]]
+            :warn-on-reflection true
+            :profiles
+            {:provided {:dependencies [[javax.servlet/servlet-api "2.5"]]}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
